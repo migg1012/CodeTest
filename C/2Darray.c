fcb678dbf0dc3b6f9fad3f54757666fa67cb5a25
Marcor Santos <77021209+marcorsantos@users.noreply.github.com>
2021-03-13 16:05:08 +0800

    struct
    struct

--- C/2Darray.c
+++ C/2Darray.c
@@ -1,33 +1,78 @@
 #include <stdio.h>
+#include <stdlib.h>
+//creates a 2 dimensional array
+//deallocate memory after using to prevent memory leaks
 
- int main()
- {
-
-    int a[100];
-
-    printf("Array address using a %p\n",a);
-    printf("Array address using &a %p\n",&a);
-    printf("Content of array using *\(a+i\) at %p\n",&a);
+int ** Array2D(int nRows, int nCols)
+{
+    int **a;
+    a = (int **)malloc(nRows * sizeof(int *));
+    if(a == NULL)
+        exit(1);
     int i;
-    for(i=0; i<5; i++)
+    for(i=0;i<nRows;i++)
     {
-        printf("%4d", *(a+i));
+        *(a+i) = (int *)malloc(nCols * sizeof(int));
+        if(*(a+i) == NULL)
+        {
+            exit(1);
+        }
     }
-    printf("\n");
 
-    printf("Content of array using a[i] at %p\n",&a);
-    for(i=0; i<5; i++)
-    {
-        printf("%4d", a[i]);
-    }
-    printf("\n");
+    return a;
+}
 
-    printf("Content of array using i[a] at %p\n",&a);
-    for(i=0; i<5; i++)
+void inputValues(int nRows, int nCols, int **a)
+{
+    int i, j;
+    for(i=0;i<nRows;i++)
     {
-        printf("%4d", i[a]);
+        for(j=0;j<nCols;j++)
+        {
+            printf("Enter value for row %d of column %d: ", i, j);
+            scanf("%d",(*(a+i)+j));
+        }
     }
-    printf("\n");
+}
+
+void printValues(int nRows, int nCols, int **a)
+{
+    int i, j;
+    for(i=0;i<nRows;i++)
+    {
+        for(j=0;j<nCols;j++)
+        {
+            printf("%8d", *(*(a+i)+j));
+        }
+        printf("\n");
+    }
+}
+
+void deAllocate(int nRows, int **a)
+{
+    int i, j;
+    for(i=0;i<nRows;i++)
+    {
+        free(*(a+i));
+    }
+}
+
+
+int main()
+{
+
+    int **p;
+
+    int nRows, nCols;
+    printf("Enter number of Rows: ");
+    scanf("%d", &nRows);
+    printf("Enter number of Columns: ");
+    scanf("%d",&nCols);
+
+    p = Array2D(nRows, nCols);
+    inputValues(nRows, nCols, p);
+    printValues(nRows, nCols, p);
+    deAllocate(nRows, p);
 
     return 0;
-    }
+}
