a31f9a48b4f996d18d6ef291da2e27555cf09d53
migg1012 <migg1012@yahoo.com>
2022-10-29 14:24:45 +0800

    recursions and loops

---
+++ C/armstrngRecur.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+
+int sum = 0; 
+
+int sumOfDigits(int num)
+{
+    int rem;
+    if(num == 0)
+    {
+        return sum;
+    }
+    if(num>0)
+    {
+        rem = num % 10;
+        num = num / 10;
+        sum = sum + (rem * rem * rem);
+        return sumOfDigits(num);
+    }
+}
+
+void getArmstrong(int num)
+{
+    if(num==1)
+    {
+        printf("%d is an armstrong number.\n", num);
+        return;
+    }
+    if(num<=100 && num > 1)
+    {
+        num = num - 1;
+        getArmstrong(num);
+        return;
+    }
+    if(num>100)
+    {
+        if(num == sumOfDigits(num))
+        {
+            printf("%d is an armstrong number.\n", num);
+            num=num-1;
+            getArmstrong(num);
+            return;
+
+        }
+    }
+    //return;
+}
+
+int main()
+{
+    int testNum = 153;
+    int cube;
+
+    getArmstrong(400);
+
+    getchar();
+
+    return 0;
+}
---
+++ C/pyramid.c
@@ -0,0 +1,56 @@
+/*
+Create a function
+pyramid on screen.
+            *
+           ***
+          *****
+         *******
+
+Input is a positive number that tells how many rows.
+
+vertical   f(x) = x + 1
+horizontal f(x) = 2x - 2
+start of print f(x) = x/2 + 1
+*/
+
+#include <stdio.h>
+int k=1;
+void drawPyramid(int height)
+{
+    for(int i = 1; height >= i; i++)
+    {
+        for(k=i;((2*height)/2)-1 >= k;k++)
+        {
+            printf("a");
+        }
+        // for(int j = 1; (2*height)-1 >= j; j++)
+        // {
+        //     printf("#");
+        // }
+        printf("\n");
+    }
+
+
+
+    return;
+}
+
+int main(int argc, char const *argv[])
+{
+    int height = 4;
+
+    drawPyramid(height);
+    printf("\n");
+    
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
