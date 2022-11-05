815b10ff47d209bc6a971227772c0f9b4cfafec6
migg1012 <migg1012@yahoo.com>
2022-11-05 21:20:48 +0800

    C# test and libary

---
+++ C#/Core Console/library/mylibrary/Class1.cs
@@ -0,0 +1,13 @@
+﻿namespace utilibrary;
+
+public static class mylibrary
+{
+    public static bool StartsWithUpper(this string? str)
+    {
+        if (string.IsNullOrWhiteSpace(str))
+            return false;
+
+        char ch = str[0];
+        return char.IsUpper(ch);
+    }
+}
